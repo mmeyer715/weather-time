8089e3d7fa0183d218f67273c8c1710151029e0c
Maria Meyer <mbean1216@icloud.com>
2022-03-16 15:24:52 -0500

    finallygit add -Agit add -A able to get info to display to screen

--- script.js
+++ script.js
@@ -1,12 +1,10 @@
 // creating variables
+const currWeather = document.getElementById('current');
 var cityList = document.getElementById('cityList')
 const apiKey = 'f23550b18591f7fe101ff3e67461858b'
-// var cityInfo = 'http://api.openweathermap.org/data/2.5/weather?q=' + userCity + '&appid=' + apiKey
 // var pullWeather = 'http://api.openweathermap.org/data/2.5/weather?lat=' + lat + '&lon=' + lon + '&appid=' + apiKey
+
 //pulling users inputed city
-// var userCity = document.getElementById('cityInput').value;
-
-
 function current() {
     var userCity = document.getElementById('cityInput').value;
     var cityInfo = 'http://api.openweathermap.org/data/2.5/weather?q=' + userCity + '&units=imperial&appid=' + apiKey
@@ -16,11 +14,16 @@ function current() {
         })
         .then(function(data) {
             console.log(data);
-            displayCurrent(data);
-        });
+            const { main, name, sys, weather } = data;
+            const div1 = document.createElement('div');
+            const edit = `
+            <h2 data-name="${name},${sys.country}">
+                <span>${name}</span>
+                <sup>${sys.country}</sup>
+            </h2>
+            `;
+            div1.innerHTML = edit;
+            currWeather.appendChild(div1)
+        })
 }
 
-function displayCurrent() {
-    
-
-}
